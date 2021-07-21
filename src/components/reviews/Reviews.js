048c551bb65547be39faef6596ed6efcf916ad6d
krae <kraewind@gmail.com>
2021-07-21 14:23:31 -0400

    commit tree ae557d892228e434750f5b4da89e2214e08bc0a9

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -5,7 +5,7 @@ class Reviews extends Component {
   render() {
     return (
       <ul>
-        {this.props.reviews.map(review => {
+        {this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId).map(review => {
           return <li key={review.id}><Review restaurantId={this.props.restaurantId} review={review} deleteReview={this.props.deleteReview}/></li>
         })}
       </ul>
